3026143f3cb5291bb2c3130b54d179fa4aebd89f
zuram2006 <zuram@bit.ge>
2024-07-15 18:27:49 +0400

    added References

---
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Reddit;
+using Reddit.Models;
+using Reddit.Repositories;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
